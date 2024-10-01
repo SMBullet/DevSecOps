bd8f54d80da483c51d257e6e52038f714a5caaf8
Mehdi Stoti <mehdistoti01@gmail.com>
2024-10-01 20:23:02 +0100

    merged auth service into develop branch

---
+++ services/service_auth/src/main/java/com/devsecops/project/controller/AuthenticationController.java
@@ -0,0 +1,47 @@
+package com.devsecops.project.controller;
+
+import com.devsecops.project.dto.LoginUserDto;
+import com.devsecops.project.dto.RegisterUserDto;
+import com.devsecops.project.model.User;
+import com.devsecops.project.response.LoginResponse;
+import com.devsecops.project.service.AuthenticationService;
+import com.devsecops.project.service.JwtService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthenticationController {
+
+    private final JwtService jwtService;
+
+
+    private final AuthenticationService authenticationService;
+
+
+    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto) {
+        User registeredUser = authenticationService.signup(registerUserDto);
+
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto) {
+        User authenticatedUser = authenticationService.authenticate(loginUserDto);
+
+        String jwtToken = jwtService.generateToken(authenticatedUser);
+
+        LoginResponse loginResponse = new LoginResponse().setToken(jwtToken).setExpiresIn(jwtService.getExpirationTime());
+
+        return ResponseEntity.ok(loginResponse);
+    }
+}
---
+++ services/service_auth/src/main/java/com/devsecops/project/enums/Role.java
@@ -0,0 +1,8 @@
+package com.devsecops.project.enums;
+
+public enum Role{
+    STUDENT,
+    TEACHER,
+    ADMIN
+
+}
