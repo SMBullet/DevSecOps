22f9afa89d7013ff4bccf4092566852449b927d1
Yassine Essaleh <123052490+Yassinom@users.noreply.github.com>
2024-10-19 18:50:52 +0100

    Update modify_hosts.sh

--- services/service_auth/modify_hosts.sh
+++ services/service_auth/modify_hosts.sh
@@ -2,7 +2,9 @@
 # modify_hosts.sh
 
 # Add the entry to /etc/hosts
+echo "copying on the file /etc/hosts 128.110.217.179 mysql-test maaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaan"
 echo "128.110.217.179 mysql-test" >> /etc/hosts
+echo "finished modifying on /etc/hosts"
 
 # Execute the main command (Java application)
 exec "$@"
